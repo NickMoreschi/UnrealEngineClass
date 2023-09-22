8fc87855e35f0702a7599a478ee8988d3cc0d49b
Nick Moreschi <moreschinick@gmail.com>
2023-09-22 00:03:40 -0400

    renamed projectile / created new actor class
    + renamed magic projectile
    + created explosive barrel skeleton class

---
+++ FirstGame/Source/FirstGame/Private/NickExplosiveBarrel.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NickExplosiveBarrel.h"
+
+// Sets default values
+ANickExplosiveBarrel::ANickExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ANickExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANickExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
